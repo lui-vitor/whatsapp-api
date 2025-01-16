a520cad9626335bdc1cd4fe95265e14f6f65108c
lui-vitor <vitor@lui.cx>
2025-01-16 18:28:30 -0300

    Update sessions.js

--- src/sessions.js
+++ src/sessions.js
@@ -99,7 +99,7 @@ const setupSession = (sessionId) => {
       puppeteer: {
         executablePath: process.env.CHROME_BIN || null,
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage', `--user-data-dir=${process.env.CHROME_USER_DATA_DIR}/${sessionId}`]
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage'] //, `--user-data-dir=${process.env.CHROME_USER_DATA_DIR}/${sessionId}`
       },
       userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
       authStrategy: localAuth
